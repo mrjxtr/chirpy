ff2e878ca24c2a21bb4404dfcf7c0dea6671dd4e
Jester Lumacad <mr.jesterlumacad@gmail.com>
2026-03-10 20:17:27 +0800

    update: add health handler and image serving

--- main.go
+++ main.go
@@ -9,11 +9,18 @@ func main() {
 	mux := http.NewServeMux()
 
 	fs := http.FileServer(http.Dir("."))
-	mux.Handle("/", fs)
+	mux.Handle("/app/", http.StripPrefix("/app", fs))
+	mux.HandleFunc("/healthz", healthHandler)
 
 	srv := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 	log.Fatal(srv.ListenAndServe())
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
