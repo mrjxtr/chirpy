b1b0a4a5856e5d8ef71523b8cb43e46d43d79876
Jester Lumacad <mr.jesterlumacad@gmail.com>
2026-03-17 09:28:42 +0800

    feat: add metrics middleware and handler

--- main.go
+++ main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
+	apiCfg := &apiConfig{}
+
 	mux := http.NewServeMux()
 
 	fs := http.FileServer(http.Dir("."))
-	mux.Handle("/app/", http.StripPrefix("/app", fs))
-	mux.HandleFunc("/healthz", healthHandler)
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fs)))
+	mux.HandleFunc("GET /metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("POST /reset", apiCfg.resetMetricsHandler)
+	mux.HandleFunc("GET /healthz", healthHandler)
 
 	srv := http.Server{
 		Addr:    ":8080",
@@ -23,4 +34,24 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	hits := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
+	w.Write([]byte(hits))
+}
+
+func (cfg *apiConfig) resetMetricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits = atomic.Int32{}
 }
